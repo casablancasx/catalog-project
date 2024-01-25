81222397ce6c59e1fc6600731bba0ad280d3149d
danilochaves <danchavesds@gmail.com>
2024-01-24 22:40:52 -0300

    Iniciando projeto

---
+++ backend/src/main/java/br/com/danilochaves/catalogproject/CatalogProjectApplication.java
@@ -0,0 +1,13 @@
+package br.com.danilochaves.catalogproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CatalogProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CatalogProjectApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/br/com/danilochaves/catalogproject/CatalogProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.danilochaves.catalogproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CatalogProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
