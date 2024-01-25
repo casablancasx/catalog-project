5d33b8946985a14b32a9514f72a692f6f402b1ab
danilochaves <danchavesds@gmail.com>
2024-01-25 18:25:34 -0300

    User Entity

---
+++ backend/src/main/java/br/com/danilochaves/catalogproject/dto/UserDTO.java
@@ -0,0 +1,42 @@
+package br.com.danilochaves.catalogproject.dto;
+
+import br.com.danilochaves.catalogproject.entities.Role;
+import br.com.danilochaves.catalogproject.entities.User;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.JoinTable;
+import jakarta.persistence.ManyToMany;
+import lombok.*;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@EqualsAndHashCode
+public class UserDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    private Set<Role> roles = new HashSet<>();
+
+    public UserDTO(Long id, String firstName, String lastName, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public UserDTO(User entity){
+        id = entity.getId();
+        firstName = entity.getFirstName();
+        lastName = entity.getLastName();
+        email = entity.getEmail();
+        entity.getRoles().forEach((role) -> this.roles.add(role));
+    }
+}
+
---
+++ backend/src/main/java/br/com/danilochaves/catalogproject/entities/User.java
@@ -0,0 +1,24 @@
+package br.com.danilochaves.catalogproject.entities;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "tb_user")
+@Data
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+
+    @ManyToMany(fetch =FetchType.EAGER)
+    @JoinTable(name = "tb_user_role", joinColumns = @JoinColumn(name = "user_id"), inverseJoinColumns = @JoinColumn(name = "role_id"))
+    private Set<Role> roles = new HashSet<>();
+}
